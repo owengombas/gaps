5684a199a5663aceb6190b6b2150cc9195692c9f
owencalvin <owen.gombas@gmail.com>
2022-10-05 11:28:08 +0200

    Initial Commit

---
+++ gaps/gapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gapsApp.swift
+//  gaps
+//
+//  Created by owen on 05.10.22.
+//
+
+import SwiftUI
+
+@main
+struct gapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
